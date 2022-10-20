75c2dd9c619a76fd0638ee0e9d5acd9c49ebb922
DevNgecu <devngecu@gmail.com>
2022-10-20 04:25:09 +0300

    configuring template

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return HttpResponse("Hello DevNgecu")
+def login(request):
+        context = {}
+        return render(request, 'myapp/index.html', context)
